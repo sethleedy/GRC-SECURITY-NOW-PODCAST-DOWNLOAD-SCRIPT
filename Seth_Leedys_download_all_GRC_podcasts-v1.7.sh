23c4adda04868dbff024ef01bc2c9bc886667b87
sethleedy <Code@SethLeedy.Name>
2016-04-02 18:15:05 -0400

    Line Feeds were set to Windows. Switched it back to Unix.
    Also updated the Last Updated date so I could commit it.

--- Seth_Leedys_download_all_GRC_podcasts-v1.7.sh
+++ Seth_Leedys_download_all_GRC_podcasts-v1.7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Created: 2012-05-25
-# Last Updated: 2016-03-29
+# Last Updated: 2016-04-02
 
 # This script will have updates at http://techblog.sethleedy.name/?p=24172 website
 #and for development @ https://github.com/sethleedy/GRC-SECURITY-NOW-PODCAST-DOWNLOAD-SCRIPT
